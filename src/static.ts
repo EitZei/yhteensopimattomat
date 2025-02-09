e8ed82aaff77b7fabe01937d414dc3966cd157e3
Maija Wirlander <maija.wirlander@gmail.com>
2025-02-09 19:20:01 +0200

    Päivitetty podin kuvaus

--- src/static.ts
+++ src/static.ts
@@ -1,2 +1,27 @@
-export const podcastDescription =
-  "Kaikkea tai kaikkia ei ole tarkoitettu olemaan yhdessä, tuntumaan samalta tai toimimaan samoin. Samalla ristiriita on luonnonvoima, joka paljastaa ennalta-aavistamattomia ajattelun uria ja luo pysäyttämätöntä liikettä. Kenties lopulta tuo voima vaikuttaa myös sen vapauttaneisiin ja syntyy uutta. Yhteensopivampaa.";
+export const podcastDescription = "Jokaisella meistä on oma uniikki taustamme, jota vasten tulkitsemme ympäröivää maailmaa ja sen tapahtumia. Ymmärtääksemme toisiamme ja toisenlaisia ajatuksia voimme käydä keskusteluja. Podissa Maija ja Antti tekevät tulkintoja omista lähtökohdistaan yhteiskunnallisista aiheista laajentaakseen omaa ymmärrystään ja virittääkseen kahvipöytäkeskusteluja (ehkä jopa etusivun juttuja). Vaikka näkökulmat voivat aluksi näyttää yhteensopimattomilta, on lopputulos keskusteluista usein jotain yhteensopivampaa...";
+
+/*
+"Kaikkea tai kaikkia ei ole tarkoitettu olemaan yhdessä, tuntumaan samalta tai toimimaan samoin. Samalla ristiriita on luonnonvoima, joka paljastaa ennalta-aavistamattomia ajattelun uria ja luo pysäyttämätöntä liikettä. Kenties lopulta tuo voima vaikuttaa myös sen vapauttaneisiin ja syntyy uutta. Yhteensopivampaa."
+
+"Miksi juuri näistä aiheista? Miksi me katsotaan olevamme oikeat ihmiset puhumaan näistä aiheista? Eli miksi nämä aiheet ja miksi me?"
+
+"Mitä tapahtuu kun tietotekniikan DI ja oikeustieteiden maisteri ottavat yhteen yhteiskunnallisten aiheiden tiimoilta." 
+
+  "voisko tää kuitenkin olla tarina siitä kuinka me ollaan tiedetty toisemme 13 vuotta ja vasta kun avattiin suumme opimme tuntemaan toisemme"
+  
+  "Kerta toisensa jälkeen Maija ja Antti päätyvät toteamaan, että keskustelemalla asioista toisten kanssa oma ymmärrys asiasta kuin asiasta kasvaa"
+
+  "Jokaisella meillä on oma uniikki tausta, jota vasten tulkitsemme maailman tapahtumia. Ymmärtääksemme toisiamme ja toisenlaisia ajatuksia käymme keskusteluja. Podillaan Maija ja Antti haluavat herätellä keskustelua yhteiskunnallisista aiheista. Laajentaakseen omaa ymmärrystään ja virittääkseen kahvipöytäkeskusteluja (ehkä jopa etusivun juttuja) aiheista, jotka ..."
+
+  "Jokaisella meistä on oma uniikki taustamme, jota vasten tulkitsemme ympäröivää maailmaa ja sen tapahtumia. Podissa Maija ja Antti keskustelevat yhteiskunnallisista aiheista ja tekevät tulkintoja omista lähtökohdistaan."
+  
+  "Jotta me emme olisi yhtensopimattomia!"
+
+  "Vaikka näkökulmamme voivat aluksi näyttää yhteensopimattomilta on lopputulos keskusteluista usein jotain yhteensopivampaa!"
+
+  "Jokaisella meistä on oma uniikki taustamme, jota vasten tulkitsemme ympäröivää maailmaa ja sen tapahtumia. Podissa Maija ja Antti keskustelevat yhteiskunnallisista aiheista ja tekevät tulkintoja omista lähtökohdistaan. Kerta toisensa jälkeen he päätyvät toteamaan, että keskustelemalla asioista toisten kanssa oma ymmärrys asiasta kuin asiasta kasvaa. Vaikka näkökulmamme voivat aluksi näyttää yhteensopimattomilta on lopputulos keskusteluista usein jotain yhteensopivampaa!"
+
+  "Jokaisella meistä on oma uniikki taustamme, jota vasten tulkitsemme ympäröivää maailmaa ja sen tapahtumia. Ymmärtääksemme toisiamme ja toisenlaisia ajatuksia käymme keskusteluja. Podissa Maija ja Antti keskustelevat yhteiskunnallisista aiheista ja tekevät tulkintoja omista lähtökohdistaan. Podillaan Maija ja Antti haluavat herätellä keskustelua yhteiskunnallisista aiheista laajentaakseen omaa ymmärrystään ja virittääkseen kahvipöytäkeskusteluja (ehkä jopa etusivun juttuja) aiheista, jotka ..."
+
+  "Jokaisella meistä on oma uniikki taustamme, jota vasten tulkitsemme ympäröivää maailmaa ja sen tapahtumia. Ymmärtääksemme toisiamme ja toisenlaisia ajatuksia voimme käydä keskusteluja. Podissa Maija ja Antti tekevät tulkintoja omista lähtökohdistaan yhteiskunnallisista aiheista laajentaakseen omaa ymmärrystään ja virittääkseen kahvipöytäkeskusteluja (ehkä jopa etusivun juttuja). Vaikka näkökulmat voivat aluksi näyttää yhteensopimattomilta, on lopputulos keskusteluista usein jotain yhteensopivampaa!"
+*/
