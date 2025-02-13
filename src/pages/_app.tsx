5db5b889c650d3c7320aa8cb2c00aa905802e979
Antti Järvinen <antti.jarvinen@alupark.fi>
2025-02-13 07:19:54 +0200

    Add favicon and og:tags. Fix light theme link color.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,18 @@
 import type { AppProps } from "next/app";
 import Layout from "../components/layout";
 import "./globals.css";
+import Head from "next/head";
+import { podcastDescription } from "@/static";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <Head>
+        <title>Yhteensopimattomat</title>
+        <link rel="icon" type="image/png" href="/favicon.png" />
+        <meta property="og:title" content="Yhteensopimattomat" />
+        <meta property="og:description" content={podcastDescription} />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
